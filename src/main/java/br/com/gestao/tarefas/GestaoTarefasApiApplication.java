085da92aaf35690bc0a9a2416f12cf208e4a1eaf
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 22:08:48 -0300

    adicionando dependencias

---
+++ src/main/java/br/com/gestao/tarefas/GestaoTarefasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestao.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoTarefasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoTarefasApiApplication.class, args);
+	}
+
+}
